f797713878600a73fcea66fa6d45f976e06a3c51
innocentemutabazi <innocentemutabazi@gmail.com>
2024-05-07 12:41:06 +0200

    .

---
+++ shortest.js
@@ -0,0 +1,20 @@
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s) {
+    const arr = s.split(' ');
+    const lengthobj = {}
+    const lengtharr = []
+    for (const arry of arr) {
+        lengthobj[arry] = arry.length
+
+        
+    }
+
+    for (key in lengthobj){
+    lengtharr.push(parseInt(lengthobj[key]))
+    }
+    return Math.min(...lengtharr)
+}
