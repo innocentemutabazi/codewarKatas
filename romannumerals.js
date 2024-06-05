7af565bd807f30fb15f8753384e46b4153e76411
innocentemutabazi <innocentemutabazi@gmail.com>
2024-06-05 21:31:11 +0200

    Started another one

--- romannumerals.js
+++ romannumerals.js
@@ -13,20 +13,92 @@ function solution(number) {
         'D': 500,
         'M': 1000,
     }
-    for (let numb of numarray){
-        if (numb.length === 4){
-            numarray[numarray.indexOf(numb)] = 'M'.repeat( Math.floor(parseInt(numb)/romanobj['M']))
+    for (let numb of numarray) {
+        if (numb.length === 4) {
+            numarray[numarray.indexOf(numb)] = 'M'.repeat(Math.floor(parseInt(numb) / romanobj['M']))
         }
-        if (numb.length === 3){
-            if (parseInt(numb)<400){
-                numarray[numarray.indexOf(numb)] = 'C '.repeat( Math.floor(parseInt(numb)/romanobj['C']))
- 
+        if (numb.length === 3) {
+            if (parseInt(numb) < 400) {
+                numarray[numarray.indexOf(numb)] = 'C '.repeat(Math.floor(parseInt(numb) / romanobj['C']))
+
             }
-            if (parseInt(numb)===4){
-                
+            if (parseInt(numb) === 400) {
+                numarray[numarray.indexOf(numb)] = 'CD'
+            }
+            if (parseInt(numb) === 500) {
+                numarray[numarray.indexOf(numb)] = 'D'
+
+            }
+            if (parseInt(numb) > 500 && parseInt(numb) !== 900) {
+                numarray[numarray.indexOf(numb)] = 'D' + 'C'.repeat((parseInt(numb) - 500) / 100)
+
+            }
+            if (parseInt(numb) === 900) {
+                numarray[numarray.indexOf(numb)] = 'CM'
+            }
+        }
+
+        if (numb.length === 3) {
+            if (parseInt(numb) < 400) {
+                numarray[numarray.indexOf(numb)] = 'C '.repeat(Math.floor(parseInt(numb) / romanobj['C']))
+
+            }
+            if (parseInt(numb) === 400) {
+                numarray[numarray.indexOf(numb)] = 'CD'
+            }
+            if (parseInt(numb) === 500) {
+                numarray[numarray.indexOf(numb)] = 'D'
+
+            }
+            if (parseInt(numb) > 500 && parseInt(numb) !== 900) {
+                numarray[numarray.indexOf(numb)] = 'D' + 'C'.repeat((parseInt(numb) - 500) / 100)
+
+            }
+        
+        }
+        if (numb.length === 2) {
+            if (parseInt(numb) < 40) {
+                numarray[numarray.indexOf(numb)] = 'X '.repeat(Math.floor(parseInt(numb) / romanobj['X']))
+
+            }
+            if (parseInt(numb) === 40) {
+                numarray[numarray.indexOf(numb)] = 'XL'
+            }
+            if (parseInt(numb) === 500) {
+                numarray[numarray.indexOf(numb)] = 'L'
+
+            }
+            if (parseInt(numb) > 50 && parseInt(numb) !== 90) {
+                numarray[numarray.indexOf(numb)] = 'L' + 'X'.repeat((parseInt(numb) - 50) / 10)
+
+            }
+            if (parseInt(numb) === 90) {
+                numarray[numarray.indexOf(numb)] = 'XC'
+            }
+        }
+        if (numb.length === 1) {
+            if (parseInt(numb) < 4) {
+                numarray[numarray.indexOf(numb)] = 'I'.repeat(Math.floor(parseInt(numb) / romanobj['X']))
+
+            }
+            if (parseInt(numb) === 4) {
+                numarray[numarray.indexOf(numb)] = 'IV'
+            }
+            if (parseInt(numb) === 5) {
+                numarray[numarray.indexOf(numb)] = 'V'
+
+            }
+            if (parseInt(numb) > 50 && parseInt(numb) !== 90) {
+                numarray[numarray.indexOf(numb)] = 'V' + 'I'.repeat(parseInt(numb) - 5)
+
+            }
+            if (parseInt(numb) === 9) {
+                numarray[numarray.indexOf(numb)] = 'IX'
             }
         }
     }
-    console.log(numarray)
+    return numarray.join('')
 }
+
+
 console.log(solution(3999))
